8b8fd725dbad292a518a6301c9053b285e18080f
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-05-06 16:24:43 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,23 +1,23 @@
 class TextAnalyzer
   attr_reader :text
-<<<<<<< HEAD
+
  
   def initialize(text)
     @text = text.downcase
   end
  
-=======
+
 
   def initialize(text)
     @text = text.downcase
   end
 
->>>>>>> 3892c49fca55a2e4baa37edc394fd3303fe9816f
+
   def count_of_words
     words = text.split(" ")
     words.count
   end
-<<<<<<< HEAD
+
  
   def count_of_vowels
     text.scan(/[aeoui]/).count
@@ -27,7 +27,7 @@ class TextAnalyzer
     text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
  
-=======
+
 
   def count_of_vowels
     text.scan(/[aeoui]/).count
@@ -37,13 +37,13 @@ class TextAnalyzer
     text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
   end
 
->>>>>>> 3892c49fca55a2e4baa37edc394fd3303fe9816f
+
   def most_used_letter
     s1 = text.gsub(/[^a-z]/, '') # gets rid of spaces
     arr = s1.split('')
     arr1 = arr.uniq
     arr2 = {}
-<<<<<<< HEAD
+
  
     arr1.map do |c|
       arr2[c] =  arr.count(c)
@@ -51,7 +51,7 @@ class TextAnalyzer
  
     biggest = { arr2.keys.first => arr2.values.first }
  
-=======
+
 
     arr1.map do |c|
       arr2[c] =  arr.count(c)
@@ -59,21 +59,15 @@ class TextAnalyzer
 
     biggest = { arr2.keys.first => arr2.values.first }
 
->>>>>>> 3892c49fca55a2e4baa37edc394fd3303fe9816f
     arr2.each do |key, value|
       if value > biggest.values.first
         biggest = {}
         biggest[key] = value
       end
     end
-<<<<<<< HEAD
+
  
     biggest
   end
 end
-=======
 
-    biggest
-  end
-end
->>>>>>> 3892c49fca55a2e4baa37edc394fd3303fe9816f
